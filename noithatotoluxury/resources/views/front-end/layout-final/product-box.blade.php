6229bba90950be5295299ad45229482ea75c52d2
hoai-nam20132674 <namnguyen20132674@gmail.com>
2019-11-15 11:50:59 +0700

    12h 15/11/19

---
+++ noithatotoluxury/resources/views/front-end/layout-final/product-box.blade.php
@@ -0,0 +1,43 @@
+<div class="product-small col has-hover post-7084 product type-product status-publish has-post-thumbnail product_cat-day-tap-dan-hoi product_cat-dung-cu-tap-ca-nhan product_tag-bo-5-day-dan-hoi product_tag-bo-day-dan-hoi-the-duc product_tag-bo-day-tap-the-thao-tai-nha product_tag-day-khang-luc product_tag-day-ngu-sac-the-duc first instock sale shipping-taxable purchasable product-type-simple">
+	<div class="col-inner">
+
+		<div class="badge-container absolute left top z-1">
+			<div class="callout badge badge-circle"><div class="badge-inner secondary on-sale"><span class="onsale">Giảm giá!</span></div></div>
+		</div>
+		<div class="product-small box ">
+			<div class="box-image">
+				<div class="image-zoom">
+					<a href="https://mdbuddy.vn/product/bo-5-day-tap-the-duc-dan-hoi-chinh-hang-mdbuddy-md1366/">
+						<img width="250" height="250" src="https://mdbuddy.vn/home/wp-content/uploads/2018/09/bo-5-day-tap-the-duc-dan-hoi-12-250x250.jpg" class="attachment-woocommerce_thumbnail size-woocommerce_thumbnail" alt="" srcset="https://mdbuddy.vn/home/wp-content/uploads/2018/09/bo-5-day-tap-the-duc-dan-hoi-12-250x250.jpg 250w, https://mdbuddy.vn/home/wp-content/uploads/2018/09/bo-5-day-tap-the-duc-dan-hoi-12-150x150.jpg 150w, https://mdbuddy.vn/home/wp-content/uploads/2018/09/bo-5-day-tap-the-duc-dan-hoi-12-300x300.jpg 300w, https://mdbuddy.vn/home/wp-content/uploads/2018/09/bo-5-day-tap-the-duc-dan-hoi-12-768x768.jpg 768w, https://mdbuddy.vn/home/wp-content/uploads/2018/09/bo-5-day-tap-the-duc-dan-hoi-12-510x510.jpg 510w, https://mdbuddy.vn/home/wp-content/uploads/2018/09/bo-5-day-tap-the-duc-dan-hoi-12-100x100.jpg 100w, https://mdbuddy.vn/home/wp-content/uploads/2018/09/bo-5-day-tap-the-duc-dan-hoi-12.jpg 900w" sizes="(max-width: 250px) 100vw, 250px" />
+					</a>
+				</div>
+				<div class="image-tools is-small top right show-on-hover"></div>
+				<div class="image-tools is-small hide-for-small bottom left show-on-hover"></div>
+				<div class="image-tools grid-tools text-center hide-for-small bottom hover-slide-in show-on-hover">
+					<a class="quick-view" data-prod="7084" href="#quick-view">Xem nhanh</a>
+				</div>
+			</div><!-- box-image -->
+
+			<div class="box-text box-text-products">
+				<div class="title-wrapper">
+					<p class="name product-title"><a href="https://mdbuddy.vn/product/bo-5-day-tap-the-duc-dan-hoi-chinh-hang-mdbuddy-md1366/">Bộ 5 dây tập thể dục đàn hồi chính hãng MDBuddy MD1366</a></p>
+				</div>
+				<div class="price-wrapper">
+					<div class="container-rating">
+						<div class="star-rating">
+							<span style="width:100%">Được xếp hạng <strong class="rating">5.00</strong> 5 sao</span>
+						</div>
+						<div class="count-rating">(2)</div>
+					</div>
+					<span class="price">
+						<del><span class="woocommerce-Price-amount amount">1.499.000<span class="woocommerce-Price-currencySymbol">&#8363;</span></span></del>
+						<ins><span class="woocommerce-Price-amount amount">899.000<span class="woocommerce-Price-currencySymbol">&#8363;</span></span></ins>
+					</span>
+				</div>
+				<div class="add-to-cart-button">
+					<a href="/?add-to-cart=7084" rel="nofollow" data-product_id="7084" class="ajax_add_to_cart add_to_cart_button product_type_simple button primary is-flat mb-0 is-small">Thêm vào giỏ</a>
+				</div>
+			</div><!-- box-text -->
+		</div><!-- box -->
+	</div><!-- .col-inner -->
+</div><!-- col -->
